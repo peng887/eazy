e54850d35422ef6b71162f602584ebb572c95a1f
peng <13910555745@139.com>
2019-01-08 11:58:32 +0800

    -a

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const electron = require('electron')
-const { app, BrowserWindow,ipcMain } = require('electron')
+const { electron, app, BrowserWindow,ipcMain } = require('electron')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
 const https = require('https')
 const versionUrl = 'https://raw.githubusercontent.com/peng887/eazy/master/package.json'
 
+//创建窗口
 let onlineStatusWindow
 const createWindow = () => {
   onlineStatusWindow = new BrowserWindow({
@@ -21,38 +21,46 @@ const createWindow = () => {
 app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required')
 app.on('ready',createWindow)
 
+//检查更新
 ipcMain.on('asynchronous-message', (event, arg) => {
   if (arg) {
+    console.info("检查更新中...")
     https.get(versionUrl,(res) => {
-      res.on("data",(data) => {
-        let versionOnline = JSON.parse(data.toString()).version
-        fs.readFile('./package.json',(err,res) => {
-          let versionOutline = JSON.parse(res.toString()).version
-          if (versionOnline != versionOutline) {
-            event.sender.send('asynchronous-reply', true)
-          } else {
-            event.sender.send('asynchronous-reply', false)
-          }
+      if (res.statusCode == 200) {
+        res.on("data",(data) => {
+          let versionOnline = JSON.parse(data.toString()).version
+          fs.readFile('./package.json',(err,res) => {
+            let versionOutline = JSON.parse(res.toString()).version
+            if (versionOnline != versionOutline) {
+              event.sender.send('asynchronous-reply', true)
+            } else {
+              event.sender.send('asynchronous-reply', false)
+            }
+          })
+        }).on('error', (e) => {
+          console.error(e)
         })
-      })
+      } else {
+        console.info(res.statusCode)
+      }
     })
   }
 })
 
 network()
 
+//macOS窗口处理
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
-})
-
-app.on('activate', () => {
+}).on('activate', () => {
   if (onlineStatusWindow === null) {
     createWindow()
   }
 })
 
+//检查网络
 function network () {
   let isOnline = true
   let isOutline = false
